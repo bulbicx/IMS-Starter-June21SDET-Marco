6e56dc45bb6bca7a251a44ee37cf5b50d1ff5987
Marco Castellana <arkanoidmark96@gmail.com>
2021-07-13 16:16:58 +0100

    Added CustomerController test

---
+++ src/test/java/com/qa/ims/persistence/domain/DomainTest.java
@@ -0,0 +1,45 @@
+package com.qa.ims.persistence.domain;
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+public class DomainTest {
+
+
+	Domain domain;
+
+	public static final Logger LOGGER = LogManager.getLogger();
+
+
+	@Test
+	public void constructorTest() {
+		
+	}
+
+	@Test
+	public void getDescriptionTest() {
+//		Domain domainCust = domain.CUSTOMER;
+//		
+//		String expectedResult = "CUSTOMER: Information about customers";
+//		String actualResult = domainCust.getDescription();
+//		
+//		assertEquals(expectedResult, actualResult);
+	}
+
+	@Test
+	public void printDomainsTest() {
+		
+	}
+
+	@Test
+	public void getDomain() {
+//		String expectedResult = "CUSTOMER";
+//		System.out.println(Domain.valueOf(expectedResult));
+//		String actualResult = domain.getDomain("CUSTOMER");
+//		assertEquals(expectedResult, )
+		
+	}
+}
